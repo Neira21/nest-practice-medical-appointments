1f67e570830c40174cdd3bf29bd8bc67e2ff096d
Alvaro Neira <alvaroneira98@gmail.com>
2025-10-01 22:48:12 -0500

    init users register and authentication

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppointmentsModule } from './appointments/appointments.module';
+import { UsersModule } from './users/users.module';
+import { RolesModule } from './roles/roles.module';
 
 @Module({
-  imports: [AppointmentsModule],
+  imports: [AppointmentsModule, UsersModule, RolesModule],
   controllers: [AppController],
   providers: [AppService],
 })
